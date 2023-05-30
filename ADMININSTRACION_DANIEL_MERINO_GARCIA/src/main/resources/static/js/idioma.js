44a943af7c7dbd7c13de3ea5e0c8238d56cb70f3
ddmg299 <danielmeringar@gmail.com>
2023-05-30 22:36:55 +0200

    cambios finales

--- ADMININSTRACION_DANIEL_MERINO_GARCIA/src/main/resources/static/js/idioma.js
+++ ADMININSTRACION_DANIEL_MERINO_GARCIA/src/main/resources/static/js/idioma.js
@@ -1,49 +1,23 @@
-console.log("Cambio de idiomaa");
-//let elementos = document.getElementsByClassName('datatable-dropdown');
-//
-//
-//let traducciones = {
-//  'entries': 'Entradas',
-//  'per': 'por',
-//  'page': 'página'
-//};
-//  
-//console.log("ingles");
-//for (let i = 0; i < elementos.length; i++) {
-//  let elemento = elementos[i];
-//  let textoEnIngles = elemento.innerHTML;
-//  console.log(textoEnIngles);
-//  
-//  if (traducciones.hasOwnProperty(textoEnIngles)) {
-//    let textoEnEspanol = traducciones[textoEnIngles];
-//    elemento.innerHTML = textoEnEspanol;
-//  }
+window.onload = function(){
+let search = document.getElementsByTagName("input");
+for (input of search){
+	if (input.type === "search")
+		input.placeholder = "Buscar...";
+}
+console.log("");
+
+//let title = document.getElementsByTagName("label");
+//for (label of title){
+//	if (label.textContent === "\n                510152025 entries per page\n            ")
+//		label.textContent = "\n                510152025 entries per page\n            ";
 //}
+//console.log("");
 
 
-// Obtener los elementos relevantes por su clase
-//var dropdown = document.querySelector('.datatable-selector');
-//var input = document.querySelector('.datatable-input');
-//
-//// Definir las traducciones de los elementos
-//var traducciones = {
-//  'entries per page': 'entradas por página',
-//  'Search...': 'Buscar...',
-//};
-//
-//// Cambiar el texto de los elementos
-//dropdown.innerHTML = dropdown.innerHTML.replace(dropdown.textContent.trim(), traducciones[dropdown.textContent.trim()]);
-//input.setAttribute('placeholder', traducciones[input.getAttribute('placeholder')]);
 
 
-// Obtener el elemento label dentro de datatable-dropdown
-let labelElement = document.querySelector('.datatable-dropdown label');
-// Verificar si se encontró el elemento
-if (labelElement) {
-	console.log("encontrado");
-  // Realizar las operaciones deseadas con el elemento label
-  // Por ejemplo, cambiar su texto
-  labelElement.textContent = 'Cambiar etiqueta';
+
+
 }
 
 
--- TIENDA_DANIEL_MERINO_GARCIA/TIENDA_DANIEL_MERINO_GARCIA/src/main/webapp/fragments/Header.jsp
+++ TIENDA_DANIEL_MERINO_GARCIA/TIENDA_DANIEL_MERINO_GARCIA/src/main/webapp/fragments/Header.jsp
@@ -8,7 +8,7 @@
 
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="<%=request.getContextPath() %>" class="nav-link px-2 text-secondary">Inicio</a></li>
+          <li><a href="<%=request.getContextPath() %>" class="nav-link px-2 text-light">Inicio</a></li>
 
         </ul>
 
